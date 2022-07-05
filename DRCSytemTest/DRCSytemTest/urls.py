3fc529c54bcaed2e7a30044a9f51dfa7ddebeec8
Het-8961 <shet8961@gmail.com>
2022-07-05 12:52:44 +0530

    SignUp Page

--- DRCSytemTest/DRCSytemTest/urls.py
+++ DRCSytemTest/DRCSytemTest/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
+    # Uncomment the next line to enable the admin:
     path('admin/', admin.site.urls),
+    path('', include('app1.url'))
 ]
--- DRCSytemTest/app1/views.py
+++ DRCSytemTest/app1/views.py
@@ -1,3 +1,21 @@
+from .forms import SignUpForm
+from .models import signUp as signUpModel
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+import logging
 
-# Create your views here.
+def signUp(request):
+    submitted = False
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            CongratsMessaage="Congrats!! You have Successfully Signed-up!"
+            print(CongratsMessaage)
+            logging.info(CongratsMessaage)
+            return render(request, 'base.html', {'error': '','success': "Success", 'successMessage':CongratsMessaage})
+    else:
+        form = SignUpForm()
+        if 'submitted' in request.GET:
+            submitted = True
+    return render(request, 'app1/signUp.html', {'form': form, 'submitted': submitted,'error': '','success':''})
